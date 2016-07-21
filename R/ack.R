7298fbd4712549417fadb115474ae8308fefde6a
Simon Garnier <garnier@njit.edu>
2016-07-21 10:35:30 +1000

    Adds sample images and videos.

---
+++ R/ack.R
@@ -0,0 +1,22 @@
+# ------
+#' Image and video samples
+#'
+#' \code{\link{Rvision}} provides a few image and video samples for illustrating
+#' the examples of its different functions. Below are references to the original
+#' material from which these samples have been extracted.
+#'
+#' @name RvisionAck
+#'
+#' @docType data
+#'
+#' @references
+#' \itemize{
+#'  \item{"Big Buck Bunny", © copyright 2008, Blender Foundation /
+#'    \link{www.bigbuckbunny.org} - Distributed under the Creative Commons
+#'    Attribution 3.0 license.}
+#'  \item{EC Funded CAVIAR project/IST 2001 37540 -
+#'    \link{http://homepages.inf.ed.ac.uk/rbf/CAVIAR/}}
+#'  \item{Pixabay / \link{https://pixabay.com/en/dots-blue-background-fabric-313325/}
+#'    - Distributed under the Creative Commons CC0 Public Domain license.}
+#' }
+NULL
