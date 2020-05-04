b66bc8bcf6f88c3dde6b69a02be2bcfd7bff1c26
Simon Garnier <garnier@njit.edu>
2020-05-03 21:25:08 -0400

    Update installation instructions

--- vignettes/z1_install.R
+++ vignettes/z1_install.R
@@ -1,13 +1,10 @@
-## ---- message=FALSE, eval=FALSE------------------------------------------
+## ---- message=FALSE, eval=FALSE-----------------------------------------------
 #  if (!require(devtools))
 #    install.packages("devtools")
 
-## ---- message=FALSE, eval=FALSE------------------------------------------
-#  devtools::install_github("swarm-lab/Rvision", ref = "v0.3.5")
-
-## ---- message=FALSE, eval=FALSE------------------------------------------
+## ---- message=FALSE, eval=FALSE-----------------------------------------------
 #  devtools::install_github("swarm-lab/Rvision")
 
-## ---- message=FALSE, eval=FALSE------------------------------------------
+## ---- message=FALSE, eval=FALSE-----------------------------------------------
 #  library(Rvision)
 
