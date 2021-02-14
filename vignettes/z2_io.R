133cdd3e2b40b40b8987eb7236926a8b8196a27f
Simon Garnier <garnier@njit.edu>
2021-02-14 14:34:18 +0100

    Update website.

--- vignettes/z2_io.R
+++ vignettes/z2_io.R
@@ -1,48 +1,48 @@
-## ----message=FALSE, warning=FALSE, include=FALSE-------------------------
+## ----message=FALSE, warning=FALSE, include=FALSE------------------------------
 library(Rvision)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  path_to_video <- system.file("sample_vid", "Balloon.mp4", package = "Rvision")
 #  my_video <- video(filename = path_to_video)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  release(my_video)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  my_stream <- stream(index = 0)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  release(my_stream)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  path_to_image <- system.file("sample_img", "bunny.png", package = "Rvision")
 #  my_image <- image(filename = path_to_image)
 
-## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE-------------
+## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE------------------
 #  path_to_video <- system.file("sample_vid", "Balloon.mp4", package = "Rvision")
 #  my_video <- video(filename = path_to_video)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  my_image <- readNext(my_video)
 
-## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE-------------
+## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE------------------
 #  my_image <- readFrame(my_video, 100)
 #  
 #  release(my_video)
 
-## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE-------------
+## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE------------------
 #  my_stream <- stream(index = 0)
 
-## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE-------------
+## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE------------------
 #  my_image <- readNext(my_stream)
 #  
 #  release(my_stream)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  path_to_video <- paste0(tempfile(), ".mp4")
 #  my_writer <- videoWriter(path_to_video, fourcc = "x264", fps = 30, height = 720, width = 1280)
 
-## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE-------------
+## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE------------------
 #  my_stream <- stream(index = 0)
 #  
 #  for (i in seq_len(30)) {
@@ -51,10 +51,10 @@ library(Rvision)
 #  
 #  release(my_stream)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  release(my_writer)
 
-## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE-------------
+## ----message=FALSE, warning=FALSE, include=FALSE, eval=FALSE------------------
 #  my_stream <- stream(index = 0)
 #  
 #  path_to_image <- paste0(tempfile(), ".png")
