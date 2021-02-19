84ae59572bb25556897114e053a4f0082eb37c39
Simon Garnier <garnier@njit.edu>
2021-02-19 11:10:25 +0100

    Simplify arithmetic code.

--- src/arithmetic.h
+++ src/arithmetic.h
@@ -1,51 +1,24 @@
-cv::Mat _outputMat(Image image1, Image image2) {
-  cv::Mat out;
-  if (image1.image.type() != image2.image.type()) {
-    switch(image1.image.channels()) {
-    case 1:
-      out = cv::Mat::zeros(image1.image.size(), CV_32F);
-      break;
-    case 2:
-      out = cv::Mat::zeros(image1.image.size(), CV_32FC2);
-      break;
-    case 3:
-      out = cv::Mat::zeros(image1.image.size(), CV_32FC3);
-      break;
-    case 4:
-      out = cv::Mat::zeros(image1.image.size(), CV_32FC4);
-      break;
-    default:
-      throw std::range_error("Not a valid image.");
-    break;
-    }
-  } else {
-    out = cv::Mat::zeros(image1.image.size(), image1.image.type());
-  }
-
-  return out;
-}
-
 Image _plus(Image image1, Image image2) {
-  cv::Mat out = _outputMat(image1, image2);
-  cv::add(image1.image, image2.image, out, cv::noArray(), out.type());
+  cv::Mat out;
+  cv::add(image1.image, image2.image, out, cv::noArray(), image1.image.type());
   return Image(out);
 }
 
 Image _minus(Image image1, Image image2) {
-  cv::Mat out = _outputMat(image1, image2);
-  cv::subtract(image1.image, image2.image, out, cv::noArray(), out.type());
+  cv::Mat out;
+  cv::subtract(image1.image, image2.image, out, cv::noArray(), image1.image.type());
   return Image(out);
 }
 
 Image _multiply(Image image1, Image image2) {
-  cv::Mat out = _outputMat(image1, image2);
-  cv::multiply(image1.image, image2.image, out, 1, out.type());
+  cv::Mat out;
+  cv::multiply(image1.image, image2.image, out, 1, image1.image.type());
   return Image(out);
 }
 
 Image _divide(Image image1, Image image2) {
-  cv::Mat out = _outputMat(image1, image2);
-  cv::divide(image1.image, image2.image, out, 1, out.type());
+  cv::Mat out;
+  cv::divide(image1.image, image2.image, out, 1, image1.image.type());
   return Image(out);
 }
 
@@ -72,13 +45,13 @@ Image _multiplyScalar(Image image, double value) {
 }
 
 Image _absdiff(Image image1, Image image2) {
-  cv::Mat out = _outputMat(image1, image2);
+  cv::Mat out;
   cv::absdiff(image1.image, image2.image, out);
   return Image(out);
 }
 
 Image _addWeighted(Image image1, double alpha, Image image2, double beta) {
-  cv::Mat out = _outputMat(image1, image2);
+  cv::Mat out;
   alpha = alpha / (alpha + beta);
   beta = 1 - alpha;
   cv::addWeighted(image1.image, alpha, image2.image, beta, 0, out);
