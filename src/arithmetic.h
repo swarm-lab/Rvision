d1802d9e3bf9185f07dc1368d2a0d43022d42642
Simon Garnier <garnier@njit.edu>
2021-03-11 10:09:40 +0100

    Allow for arithmetic operations between images with different bit depth.

--- src/arithmetic.h
+++ src/arithmetic.h
@@ -1,40 +1,40 @@
 void _plus(Image& image1, Image& image2, Image& target) {
-  cv::add(image1.image, image2.image, target.image, cv::noArray(), -1);
+  cv::add(image1.image, image2.image, target.image, cv::noArray(), target.image.depth());
 }
 
 void _plusScalar(Image& image, Rcpp::NumericVector value, Image& target) {
-  cv::add(image.image, col2Scalar(value), target.image, cv::noArray(), -1);
+  cv::add(image.image, col2Scalar(value), target.image, cv::noArray(), target.image.depth());
 }
 
 void _minus(Image& image1, Image& image2, Image& target) {
-  cv::subtract(image1.image, image2.image, target.image, cv::noArray(), -1);
+  cv::subtract(image1.image, image2.image, target.image, cv::noArray(), target.image.depth());
 }
 
 void _minusScalar(Image& image, Rcpp::NumericVector value, bool order, Image& target) {
   if (order) {
-    cv::subtract(image.image, col2Scalar(value), target.image, cv::noArray(), -1);
+    cv::subtract(image.image, col2Scalar(value), target.image, cv::noArray(), target.image.depth());
   } else {
-    cv::subtract(col2Scalar(value), image.image, target.image, cv::noArray(), -1);
+    cv::subtract(col2Scalar(value), image.image, target.image, cv::noArray(), target.image.depth());
   }
 }
 
 void _multiply(Image& image1, Image& image2, Image& target) {
-  cv::multiply(image1.image, image2.image, target.image, 1, -1);
+  cv::multiply(image1.image, image2.image, target.image, 1, target.image.depth());
 }
 
 void _multiplyScalar(Image& image, Rcpp::NumericVector value, Image& target) {
-  cv::multiply(image.image, col2Scalar(value), target.image, 1, -1);
+  cv::multiply(image.image, col2Scalar(value), target.image, 1, target.image.depth());
 }
 
 void _divide(Image& image1, Image& image2, Image& target) {
-  cv::divide(image1.image, image2.image, target.image, 1, -1);
+  cv::divide(image1.image, image2.image, target.image, 1, target.image.depth());
 }
 
 void _divideScalar(Image& image, Rcpp::NumericVector value, bool order, Image& target) {
   if (order) {
-    cv::divide(image.image, col2Scalar(value), target.image, 1, -1);
+    cv::divide(image.image, col2Scalar(value), target.image, 1, target.image.depth());
   } else {
-    cv::divide(col2Scalar(value), image.image, target.image, 1, -1);
+    cv::divide(col2Scalar(value), image.image, target.image, 1, target.image.depth());
   }
 }
 
@@ -45,5 +45,5 @@ void _absdiff(Image& image1, Image& image2, Image& target) {
 void _addWeighted(Image& image1, double alpha, Image& image2, double beta, Image& target) {
   alpha = alpha / (alpha + beta);
   beta = 1 - alpha;
-  cv::addWeighted(image1.image, alpha, image2.image, beta, 0, target.image);
+  cv::addWeighted(image1.image, alpha, image2.image, beta, 0, target.image, target.image.depth());
 }
