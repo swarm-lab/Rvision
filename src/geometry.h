56e31bf99db037dd8142066eb705d03f40f8660b
Simon Garnier <garnier@njit.edu>
2021-03-15 22:38:01 +0100

    Flip and resize are GPU ready.

--- src/geometry.h
+++ src/geometry.h
@@ -1,8 +1,20 @@
 void _resize(Image& image, int height, int width, double fx, double fy,
               int interpolation, Image& target) {
-  cv::resize(image.image, target.image, cv::Size(width, height), fx, fy, interpolation);
+  if (image.GPU && target.GPU) {
+    cv::resize(image.uimage, target.uimage, cv::Size(width, height), fx, fy, interpolation);
+  } else if (!image.GPU && !target.GPU) {
+    cv::resize(image.image, target.image, cv::Size(width, height), fx, fy, interpolation);
+  } else {
+    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
+  }
 }
 
 void _flip(Image& image, int flipCode, Image& target) {
-  cv::flip(image.image, target.image, flipCode);
+  if (image.GPU && target.GPU) {
+    cv::flip(image.uimage, target.uimage, flipCode);
+  } else if (!image.GPU && !target.GPU) {
+    cv::flip(image.image, target.image, flipCode);
+  } else {
+    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
+  }
 }
