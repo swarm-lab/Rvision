63eb3f484fe533163b90b48271d25f48b736d1fb
Simon Garnier <garnier@njit.edu>
2021-03-16 16:14:05 +0100

    Logical functions are GPU ready.

--- src/logical.h
+++ src/logical.h
@@ -1,26 +1,61 @@
 void _and(Image& image1, Image& image2, Image& target) {
-  cv::bitwise_and(image1.image, image2.image, target.image);
+  if (image1.GPU && image2.GPU && target.GPU) {
+    cv::bitwise_and(image1.uimage, image2.uimage, target.uimage);
+  } else if (!image1.GPU && !image2.GPU && !target.GPU) {
+    cv::bitwise_and(image1.image, image2.image, target.image);
+  } else {
+    Rcpp::stop("'e1$GPU', 'e2$GPU', and 'target$GPU' are not equal.");
+  }
 }
 
-void _andScalar(Image& image1, Rcpp::NumericVector value, Image& target) {
-  cv::bitwise_and(image1.image, col2Scalar(value), target.image);
+void _andScalar(Image& image, Rcpp::NumericVector value, Image& target) {
+  if (image.GPU && target.GPU) {
+    cv::bitwise_and(image.uimage, col2Scalar(value), target.uimage);
+  } else if (!image.GPU && !target.GPU) {
+    cv::bitwise_and(image.image, col2Scalar(value), target.image);
+  } else {
+    Rcpp::stop("'e1$GPU' (or 'e2$GPU') and 'target$GPU' are not equal.");
+  }
 }
 
 void _or(Image& image1, Image& image2, Image& target) {
-  cv::bitwise_or(image1.image, image2.image, target.image);
+  if (image1.GPU && image2.GPU && target.GPU) {
+    cv::bitwise_or(image1.uimage, image2.uimage, target.uimage);
+  } else if (!image1.GPU && !image2.GPU && !target.GPU) {
+    cv::bitwise_or(image1.image, image2.image, target.image);
+  } else {
+    Rcpp::stop("'e1$GPU', 'e2$GPU', and 'target$GPU' are not equal.");
+  }
 }
 
-void _orScalar(Image& image1, Rcpp::NumericVector value, Image& target) {
-  cv::bitwise_or(image1.image, col2Scalar(value), target.image);
+void _orScalar(Image& image, Rcpp::NumericVector value, Image& target) {
+  if (image.GPU && target.GPU) {
+    cv::bitwise_or(image.uimage, col2Scalar(value), target.uimage);
+  } else if (!image.GPU && !target.GPU) {
+    cv::bitwise_or(image.image, col2Scalar(value), target.image);
+  } else {
+    Rcpp::stop("'e1$GPU' (or 'e2$GPU') and 'target$GPU' are not equal.");
+  }
 }
 
 void _not(Image& image, Image& target) {
-  cv::bitwise_not(image.image, target.image);
+  if (image.GPU && target.GPU) {
+    cv::bitwise_not(image.uimage, target.uimage);
+  } else if (!image.GPU && !target.GPU) {
+    cv::bitwise_not(image.image, target.image);
+  } else {
+    Rcpp::stop("'e1$GPU' and 'target$GPU' are not equal.");
+  }
 }
 
 Rcpp::IntegerMatrix _findNonZero(Image& image) {
   cv::Mat locs;
-  cv::findNonZero(image.image, locs);
+
+  if (image.GPU) {
+    cv::findNonZero(image.uimage, locs);
+  } else {
+    cv::findNonZero(image.image, locs);
+  }
 
   Rcpp::IntegerMatrix out(locs.rows, 2);
   colnames(out) = Rcpp::CharacterVector::create("x", "y");
@@ -35,7 +70,12 @@ Rcpp::IntegerMatrix _findNonZero(Image& image) {
 
 Rcpp::NumericMatrix _findNonZeroVAL(Image& image) {
   cv::Mat locs;
-  cv::findNonZero(image.image, locs);
+
+  if (image.GPU) {
+    cv::findNonZero(image.uimage, locs);
+  } else {
+    cv::findNonZero(image.image, locs);
+  }
 
   Rcpp::NumericMatrix out(locs.rows, 3);
   colnames(out) = Rcpp::CharacterVector::create("x", "y", "value");
