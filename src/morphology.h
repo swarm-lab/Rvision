1903a7aebf149008e792a8efc89e417b8a7fae38
Simon Garnier <garnier@njit.edu>
2021-03-17 09:20:02 +0100

    Morphology operations are not GPU ready.

--- src/morphology.h
+++ src/morphology.h
@@ -1,28 +1,26 @@
 void _morph(Image& image, int operation, int k_shape, int k_height, int k_width,
             int iterations, Image& target) {
   cv::Mat k = cv::getStructuringElement(k_shape, cv::Size(2 * k_width + 1, 2 * k_height + 1));
-  cv::morphologyEx(image.image, target.image, operation, k, cv::Point(-1, -1), iterations);
+
+  if (image.GPU && target.GPU) {
+    cv::morphologyEx(image.uimage, target.uimage, operation, k, cv::Point(-1, -1), iterations);
+  } else if (!image.GPU && !target.GPU) {
+    cv::morphologyEx(image.image, target.image, operation, k, cv::Point(-1, -1), iterations);
+  } else {
+    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
+  }
 }
 
-void _morphCustom(Image& image, int operation, Rcpp::NumericVector kernel,
+void _morphCustom(Image& image, int operation, arma::Mat<uchar> kernel,
                   int iterations, Image& target) {
-  cv::Mat k;
-  Rcpp::IntegerVector kernelDims = kernel.attr("dim");
-  k.create(kernelDims[0], kernelDims[1], CV_8U);
+  cv::Mat_<uchar> k;
+  arma2cv(kernel, k);
 
-  if (operation != 7) {
-    for(int i = 0; i < kernelDims[0]; i++) {
-      for(int j = 0; j < kernelDims[1]; j++) {
-        k.at<unsigned char>(i, j) = kernel[kernelDims[0] * j + i];
-      }
-    }
+  if (image.GPU && target.GPU) {
+    cv::morphologyEx(image.uimage, target.uimage, operation, k, cv::Point(-1, -1), iterations);
+  } else if (!image.GPU && !target.GPU) {
+    cv::morphologyEx(image.image, target.image, operation, k, cv::Point(-1, -1), iterations);
   } else {
-    for(int i = 0; i < kernelDims[0]; i++) {
-      for(int j = 0; j < kernelDims[1]; j++) {
-        k.at<int>(i, j) = kernel[kernelDims[0] * j + i];
-      }
-    }
+    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
   }
-
-  cv::morphologyEx(image.image, target.image, operation, k, cv::Point(-1, -1), iterations);
 }
