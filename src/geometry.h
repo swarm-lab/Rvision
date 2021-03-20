bf1482d5533853510b19b0bc39cccacf602fab38
Simon Garnier <garnier@njit.edu>
2021-03-20 13:38:46 +0100

    Geometry functions can handle all GPU-CPU cases.

--- src/geometry.h
+++ src/geometry.h
@@ -1,20 +1,28 @@
 void _resize(Image& image, int height, int width, double fx, double fy,
               int interpolation, Image& target) {
-  if (image.GPU && target.GPU) {
-    cv::resize(image.uimage, target.uimage, cv::Size(width, height), fx, fy, interpolation);
-  } else if (!image.GPU && !target.GPU) {
-    cv::resize(image.image, target.image, cv::Size(width, height), fx, fy, interpolation);
-  } else {
-    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
+  if (image.GPU) {
+    if (target.GPU)
+      return cv::resize(image.uimage, target.uimage, cv::Size(width, height), fx, fy, interpolation);
+
+    return cv::resize(image.uimage, target.image, cv::Size(width, height), fx, fy, interpolation);
   }
+
+  if (target.GPU)
+    return cv::resize(image.image, target.uimage, cv::Size(width, height), fx, fy, interpolation);
+
+  cv::resize(image.image, target.image, cv::Size(width, height), fx, fy, interpolation);
 }
 
 void _flip(Image& image, int flipCode, Image& target) {
-  if (image.GPU && target.GPU) {
-    cv::flip(image.uimage, target.uimage, flipCode);
-  } else if (!image.GPU && !target.GPU) {
-    cv::flip(image.image, target.image, flipCode);
-  } else {
-    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
+  if (image.GPU) {
+    if (target.GPU)
+      return cv::flip(image.uimage, target.uimage, flipCode);
+
+    return cv::flip(image.uimage, target.image, flipCode);
   }
+
+  if (target.GPU)
+    return cv::flip(image.image, target.uimage, flipCode);
+
+  cv::flip(image.image, target.image, flipCode);
 }
