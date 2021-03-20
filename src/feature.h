40e80299e245a2093af434d44e3dff8fc3dfd156
Simon Garnier <garnier@njit.edu>
2021-03-20 13:34:17 +0100

    Feature functions can handle all GPU-CPU cases.

--- src/feature.h
+++ src/feature.h
@@ -1,10 +1,14 @@
 void _canny(Image& image, double threshold1, double threshold2, int apertureSize,
              bool L2gradient, Image& target) {
-  if (image.GPU && target.GPU) {
-    cv::Canny(image.uimage, target.uimage, threshold1, threshold2, apertureSize, L2gradient);
-  } else if (!image.GPU && !target.GPU) {
-    cv::Canny(image.image, target.image, threshold1, threshold2, apertureSize, L2gradient);
-  } else {
-    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
+  if (image.GPU) {
+    if (target.GPU)
+      return cv::Canny(image.uimage, target.uimage, threshold1, threshold2, apertureSize, L2gradient);
+
+    return cv::Canny(image.uimage, target.image, threshold1, threshold2, apertureSize, L2gradient);
   }
+
+  if (target.GPU)
+    return cv::Canny(image.image, target.uimage, threshold1, threshold2, apertureSize, L2gradient);
+
+  cv::Canny(image.image, target.image, threshold1, threshold2, apertureSize, L2gradient);
 }
