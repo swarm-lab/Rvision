75d88ec559e9ff1d65c18ebd1d66087b8744f2db
Simon Garnier <garnier@njit.edu>
2021-03-20 12:59:42 +0100

    Blob and comparison functions can handle all GPU-CPU cases.

--- src/comparisons.h
+++ src/comparisons.h
@@ -1,49 +1,139 @@
 void _compare(Image& image1, Image& image2, int comp, Image& target) {
-  if (image1.GPU && image2.GPU && target.GPU) {
-    cv::compare(image1.uimage, image2.uimage, target.uimage, comp);
-  } else if (!image1.GPU && !image2.GPU && !target.GPU) {
-    cv::compare(image1.image, image2.image, target.image, comp);
-  } else {
-    Rcpp::stop("'e1$GPU', 'e2$GPU', and 'target$GPU' are not equal.");
+  if (image1.GPU) {
+    if (image2.GPU) {
+      if (target.GPU)
+        return cv::compare(image1.uimage, image2.uimage, target.uimage, comp);
+
+      return cv::compare(image1.uimage, image2.uimage, target.image, comp);
+    }
+
+    if (target.GPU)
+      return cv::compare(image1.uimage, image2.image, target.uimage, comp);
+
+    return cv::compare(image1.uimage, image2.image, target.image, comp);
   }
+
+  if (image2.GPU) {
+    if (target.GPU)
+      return cv::compare(image1.image, image2.uimage, target.uimage, comp);
+
+    return cv::compare(image1.image, image2.uimage, target.image, comp);
+  }
+
+  if (target.GPU)
+    return cv::compare(image1.image, image2.image, target.uimage, comp);
+
+  cv::compare(image1.image, image2.image, target.image, comp);
 }
 
 void _compareScalar(Image& image, Rcpp::NumericVector value, int comp, Image& target) {
-  if (image.GPU && target.GPU) {
-    cv::compare(image.uimage, col2Scalar(value), target.uimage, comp);
-  } else if (!image.GPU && !target.GPU) {
-    cv::compare(image.image, col2Scalar(value), target.image, comp);
-  } else {
-    Rcpp::stop("'e1$GPU' (or 'e2$GPU') and 'target$GPU' are not equal.");
+  if (image.GPU) {
+    if (target.GPU)
+      return cv::compare(image.uimage, col2Scalar(value), target.uimage, comp);
+
+    return cv::compare(image.uimage, col2Scalar(value), target.image, comp);
   }
+
+  if (target.GPU)
+    return cv::compare(image.image, col2Scalar(value), target.uimage, comp);
+
+  cv::compare(image.image, col2Scalar(value), target.image, comp);
 }
 
 void _inRange(Image& image, Rcpp::NumericVector low, Rcpp::NumericVector up, Image& target) {
-  if (image.GPU && target.GPU) {
-    cv::inRange(image.uimage, col2Scalar(low), col2Scalar(up), target.uimage);
-  } else if (!image.GPU && !target.GPU) {
-    cv::inRange(image.image, col2Scalar(low), col2Scalar(up), target.image);
-  } else {
-    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
+  if (image.GPU) {
+    if (target.GPU)
+      return cv::inRange(image.uimage, col2Scalar(low), col2Scalar(up), target.uimage);
+
+    return cv::inRange(image.uimage, col2Scalar(low), col2Scalar(up), target.image);
   }
+
+  if (target.GPU)
+    return cv::inRange(image.image, col2Scalar(low), col2Scalar(up), target.uimage);
+
+  cv::inRange(image.image, col2Scalar(low), col2Scalar(up), target.image);
 }
 
 void _matchTemplate(Image& image, Image& templ, int method, Image& mask, Image& target) {
-  if (image.GPU && templ.GPU && target.GPU) {
-    cv::matchTemplate(image.uimage, templ.image, target.uimage, method, mask.image);
-  } else if (!image.GPU && !templ.GPU && !target.GPU) {
-    cv::matchTemplate(image.image, templ.image, target.image, method, mask.image);
-  } else {
-    Rcpp::stop("'image$GPU', 'templ$GPU', and 'target$GPU' are not equal.");
+  if (image.GPU) {
+    if (templ.GPU) {
+      if (mask.GPU) {
+        if (target.GPU)
+          return cv::matchTemplate(image.uimage, templ.uimage, target.uimage, method, mask.uimage);
+
+        return cv::matchTemplate(image.uimage, templ.uimage, target.image, method, mask.uimage);
+      }
+
+      if (target.GPU)
+        return cv::matchTemplate(image.uimage, templ.uimage, target.uimage, method, mask.image);
+
+      return cv::matchTemplate(image.uimage, templ.uimage, target.image, method, mask.image);
+    }
+
+    if (mask.GPU) {
+      if (target.GPU)
+        return cv::matchTemplate(image.uimage, templ.image, target.uimage, method, mask.uimage);
+
+      return cv::matchTemplate(image.uimage, templ.image, target.image, method, mask.uimage);
+    }
+
+    if (target.GPU)
+      return cv::matchTemplate(image.uimage, templ.image, target.uimage, method, mask.image);
+
+    return cv::matchTemplate(image.uimage, templ.image, target.image, method, mask.image);
   }
+
+  if (templ.GPU) {
+    if (mask.GPU) {
+      if (target.GPU)
+        return cv::matchTemplate(image.image, templ.uimage, target.uimage, method, mask.uimage);
+
+      return cv::matchTemplate(image.image, templ.uimage, target.image, method, mask.uimage);
+    }
+
+    if (target.GPU)
+      return cv::matchTemplate(image.image, templ.uimage, target.uimage, method, mask.image);
+
+    return cv::matchTemplate(image.image, templ.uimage, target.image, method, mask.image);
+  }
+
+  if (mask.GPU) {
+    if (target.GPU)
+      return cv::matchTemplate(image.image, templ.image, target.uimage, method, mask.uimage);
+
+    return cv::matchTemplate(image.image, templ.image, target.image, method, mask.uimage);
+  }
+
+  if (target.GPU)
+    return cv::matchTemplate(image.image, templ.image, target.uimage, method, mask.image);
+
+  return cv::matchTemplate(image.image, templ.image, target.image, method, mask.image);
 }
 
 void _matchTemplateNoMask(Image& image, Image& templ, int method, Image& target) {
-  if (image.GPU && templ.GPU && target.GPU) {
-    cv::matchTemplate(image.uimage, templ.image, target.uimage, method, cv::noArray());
-  } else if (!image.GPU && !templ.GPU && !target.GPU) {
-    cv::matchTemplate(image.image, templ.image, target.image, method, cv::noArray());
-  } else {
-    Rcpp::stop("'image$GPU', 'templ$GPU', and 'target$GPU' are not equal.");
+  if (image.GPU) {
+    if (templ.GPU) {
+      if (target.GPU)
+        return cv::matchTemplate(image.uimage, templ.uimage, target.uimage, method, cv::noArray());
+
+      return cv::matchTemplate(image.uimage, templ.uimage, target.image, method, cv::noArray());
+    }
+
+    if (target.GPU)
+      return cv::matchTemplate(image.uimage, templ.image, target.uimage, method, cv::noArray());
+
+    return cv::matchTemplate(image.uimage, templ.image, target.image, method, cv::noArray());
   }
+
+  if (templ.GPU) {
+    if (target.GPU)
+      return cv::matchTemplate(image.image, templ.uimage, target.uimage, method, cv::noArray());
+
+    return cv::matchTemplate(image.image, templ.uimage, target.image, method, cv::noArray());
+  }
+
+  if (target.GPU)
+    return cv::matchTemplate(image.image, templ.image, target.uimage, method, cv::noArray());
+
+  cv::matchTemplate(image.image, templ.image, target.image, method, cv::noArray());
 }
