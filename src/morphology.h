9a4b55ba44fd73e166a26b2f57454c2cda43e4e1
Simon Garnier <garnier@njit.edu>
2021-03-20 20:48:49 +0100

    Morphology functions can handle all GPU-CPU cases.

--- src/morphology.h
+++ src/morphology.h
@@ -1,26 +1,36 @@
 void _morph(Image& image, int operation, int k_shape, int k_height, int k_width,
             int iterations, Image& target) {
   cv::Mat k = cv::getStructuringElement(k_shape, cv::Size(2 * k_width + 1, 2 * k_height + 1));
+  cv::Point p = cv::Point(-1, -1);
 
-  if (image.GPU && target.GPU) {
-    cv::morphologyEx(image.uimage, target.uimage, operation, k, cv::Point(-1, -1), iterations);
-  } else if (!image.GPU && !target.GPU) {
-    cv::morphologyEx(image.image, target.image, operation, k, cv::Point(-1, -1), iterations);
-  } else {
-    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
+  if (image.GPU) {
+    if (target.GPU)
+      return cv::morphologyEx(image.uimage, target.uimage, operation, k, p, iterations);
+
+    return cv::morphologyEx(image.uimage, target.image, operation, k, p, iterations);
   }
+
+  if (target.GPU)
+    return cv::morphologyEx(image.image, target.uimage, operation, k, p, iterations);
+
+  cv::morphologyEx(image.image, target.image, operation, k, p, iterations);
 }
 
 void _morphCustom(Image& image, int operation, arma::Mat<uchar> kernel,
                   int iterations, Image& target) {
   cv::Mat_<uchar> k;
   arma2cv(kernel, k);
+  cv::Point p = cv::Point(-1, -1);
 
-  if (image.GPU && target.GPU) {
-    cv::morphologyEx(image.uimage, target.uimage, operation, k, cv::Point(-1, -1), iterations);
-  } else if (!image.GPU && !target.GPU) {
-    cv::morphologyEx(image.image, target.image, operation, k, cv::Point(-1, -1), iterations);
-  } else {
-    Rcpp::stop("'image$GPU' and 'target$GPU' are not equal.");
+  if (image.GPU) {
+    if (target.GPU)
+      return cv::morphologyEx(image.uimage, target.uimage, operation, k, p, iterations);
+
+    return cv::morphologyEx(image.uimage, target.image, operation, k, p, iterations);
   }
+
+  if (target.GPU)
+    return cv::morphologyEx(image.image, target.uimage, operation, k, p, iterations);
+
+  cv::morphologyEx(image.image, target.image, operation, k, p, iterations);
 }
